98a72fad06b20f30bde74629d445d984b720e541
solazio <ovidiu.suciu@canonical.com>
2020-02-06 14:12:52 +0000

    Update to v 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="together-we-win",
-    version="0.0.1",
+    version="0.0.2",
     author="Ovidiu Suciu",
     author_email="ovidiu.suciu@canonical.com",
     py_modules=["test_snap"],
