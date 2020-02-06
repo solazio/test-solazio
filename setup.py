3058a87bef05482eeab079448abba4d6a495e2a7
solazio <ovidiu.suciu@canonical.com>
2020-02-06 13:42:41 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="snap-together",
+    version="0.0.1",
+    author="Ovidiu Suciu",
+    author_email="ovidiu.suciu@canonical.com",
+    py_modules=["test_snap"],
+)
